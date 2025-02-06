6afd0e5e4700efe95888b6479e3100cb241ba456
addjbbb <dem.djebbar@gmail.com>
2025-02-06 22:17:58 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+    function App() {
+      return (
+        <div className="App">
+          <header className="App-header">
+            <button id="bonjourButton">Cliquez ici pour Bonjour</button>
+            <button id="bonsoirButton">Cliquez ici pour Bonsoir</button>
+          </header>
+        </div>
+      );
+    }
+
+    export default App;
